615254714ffe736ac08524715ca54e5380bac342
luodani272 <luodani@myvuw.ac.nz>
2026-08-03 18:16:56 +1200

    cleaned up small mistakes

--- DATA501-2026-Assignment1/predict_model_B.R
+++ DATA501-2026-Assignment1/predict_model_B.R
@@ -1,12 +1,11 @@
 
-predict_model_A <- function(x, par) {
+predict_model_B <- function(x, par) {
   
-  a <- par[1]
-  b <- par[2]
-  c <- par[3]
-  A <- par[4]
+  b <- par[1]
+  c <- par[2]
+  A <- par[3]
   B <- par[5]
   freq <- par[6] 
 
-  a*x**2 + b*x+c +  A*sin(2*freq*pi*x) + B*cos(2*freq*pi*x) 
+  b*x+c +  A*sin(2*freq*pi*x) + B*cos(2*freq*pi*x) 
 }
--- DATA501-2026-Assignment1/run_script_B.R
+++ DATA501-2026-Assignment1/run_script_B.R
@@ -1,7 +1,7 @@
 # run the main program 
 library(ggplot2)
-source("fit_nonlinear.R")
-source("predict_model_A.R")
+source("fit_nonlinear_B.R")
+source("predict_model_B.R")
 source("plot_model.R")
 source("pipeline.R")
 
@@ -13,7 +13,7 @@ x <- my_data$x
 y <- my_data$y
 
 # set initial parameters
-start_par <-  data.frame(a = c(0.001), b=c(0.0001), c=c(0.1),A=c(0.1),B= c(0.1), freq=c(4.5), model_type=c("A") )  
+start_par <-  data.frame(b=c(0.0001), c=c(0.1),A=c(0.1),B= c(0.1), freq=c(4.5), model_type=c("B") )  
 
 
 
@@ -24,5 +24,5 @@ temp <- run_analysis(x,y,start_par)
 print(temp$plot)
 
 #print out
-print(paste("model A. BIC=",round(temp$bic,3) ," AIC=",round(temp$aic,3)))
+print(paste("model B. BIC=",round(temp$bic,3) ," AIC=",round(temp$aic,3)))
 
