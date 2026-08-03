35d7d2c9b4e671c32d493874cba0c0c97bad311f
luodani272 <luodani@myvuw.ac.nz>
2026-08-03 18:19:12 +1200

    correct minor mistake, wrong index number for inputs

--- DATA501-2026-Assignment1/predict_model_B.R
+++ DATA501-2026-Assignment1/predict_model_B.R
@@ -4,8 +4,8 @@ predict_model_B <- function(x, par) {
   b <- par[1]
   c <- par[2]
   A <- par[3]
-  B <- par[5]
-  freq <- par[6] 
+  B <- par[4]
+  freq <- par[5] 
 
   b*x+c +  A*sin(2*freq*pi*x) + B*cos(2*freq*pi*x) 
 }
