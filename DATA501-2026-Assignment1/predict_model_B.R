d4b9ae08a245d66ae43b206568785699440a68a9
luodani272 <luodani@myvuw.ac.nz>
2026-08-04 19:32:38 +1200

    keep dataframe the same, the model selects what it needs

--- DATA501-2026-Assignment1/predict_model_B.R
+++ DATA501-2026-Assignment1/predict_model_B.R
@@ -1,11 +1,12 @@
 
 predict_model_B <- function(x, par) {
   
-  b <- par[1]
-  c <- par[2]
-  A <- par[3]
-  B <- par[4]
-  freq <- par[5] 
+  a <- par[1]
+  b <- par[2]
+  c <- par[3]
+  A <- par[4]
+  B <- par[5]
+  freq <- par[6] 
 
   b*x+c +  A*sin(2*freq*pi*x) + B*cos(2*freq*pi*x) 
 }
