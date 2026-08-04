f4e18dc6a45a9e248a9f1a1d28d2717417252db2
luodani272 <luodani@myvuw.ac.nz>
2026-08-05 00:22:06 +1200

    small typo

--- DATA501-2026-Assignment1/fit_nonlinear.R
+++ DATA501-2026-Assignment1/fit_nonlinear.R
@@ -2,21 +2,21 @@ fit_nonlinear <- function(x, y, start_par) {
   
   n <- length(y)
   
-  model_type <- start_par$model_type
+  model_type <- start_par$model_type 
   
   print(paste("Model type", model_type))
   
-  if (model_type == 'B')
+  if (model_type == 'A')
   {
      # initialize
-     init_par <- c(start_par$a, start_par$b, start_par$c, start_par$A, start_par$B, start_par$freq)
+     init_par <- c(start_par$a,start_par$b, start_par$c, start_par$A, start_par$B, start_par$freq)
      
      # number of parameters
      k <- length(init_par)
      
      # objective function
      objective_function <- function(par) {
-      y_pred <- predict_model_B(x, par)
+      y_pred <- predict_model_A(x, par)
       sum((y - y_pred)^2)
       }
     
@@ -24,7 +24,7 @@ fit_nonlinear <- function(x, y, start_par) {
      result <- optim(init_par, objective_function)
    
      # get model
-     result$model <- predict_model_B(x, result$par) 
+     result$model <- predict_model_A(x, result$par) 
      
      # AIC
      result$aic <- sum((y - result$model)^2) + 2*k
